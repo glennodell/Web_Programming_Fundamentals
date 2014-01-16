3ea64a2ad192e043e1102efe25f1628ffa9bbdd6
glennodell <glennodell@fullsail.edu>
2014-01-16 08:19:05 -0600

    shopping bill

---
+++ Expressions_worksheet/JS/shoppingBill.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+// Glenn O'Dell 1-16-14 Expressions_Worksheet
+
+var fiveWeeks = [175, 200, 214, 225, 238];
+
+var total = fiveWeeks[0] + fiveWeeks[1] + fiveWeeks[2] + fiveWeeks[3] + fiveWeeks[4];
+
+var Average = total / 5;
+
+console.log("You have spent a total of $" + total + " on groceries over 5 weeks. That is an anverage of $" + Average + " per week");
