7eaa9f82cf5b9e8faecbd349ed1044a0a957bedb
glennodell <glennodell@fullsail.edu>
2014-01-16 18:45:15 -0600

    edit number 3

--- expressions_wacky/JS/wacky.js
+++ expressions_wacky/JS/wacky.js
@@ -1,20 +1,19 @@
 // Glenn O'Dell 1-16-2014 expressions_wacky
 
-var name = prompt("What is your name mortal??");
+var name =prompt("What is your name mortal??");
 
-var class = prompt("What is your class, Tiny human");
 
-var hit = prompt("How hard can you hit \n 1-20");
+var hit =prompt("How hard can you hit \n 1-20");
 
-var health = prompt("How much can you take \n 1-40");
+var health =prompt("How much can you take \n 1-40");
 
-var armor = prompt("how much armor do you have on \n 1-30");
+var armor =prompt("how much armor do you have on \n 1-30");
 
-var bossH = prompt("Number of epicness \n 1-20 please");
+var bossH =prompt("Number of epicness \n 1-20 please");
 
-var bossHit = prompt("number of greatness /n 1-20 please");
+var bossHit =prompt("number of greatness \n 1-20 please");
 
-alert("The tiny humans name is " + name + "huh \n and its a " + class + " I see");
+alert("The tiny humans name is " + name + " huh");
 
 var bad = health + armor;
 
