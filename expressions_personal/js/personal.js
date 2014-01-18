dea7d19a9af55fe2cb5d7f9dd45745a1bdf8c5af
glennodell <glennodell@fullsail.edu>
2014-01-17 22:08:46 -0600

    more math added

--- expressions_personal/js/personal.js
+++ expressions_personal/js/personal.js
@@ -16,3 +16,7 @@ alert("You can go " + distance + " per tank of fuel \n aEach tank will cost you 
 var howFar = prompt("How many miles is your trip?")
 
 var howMany = howFar / distance
+
+var howMuch = pertank / howMany
+
+console.log("You will have to fill up " + howMany + " times to get that far. \n and that will cost you " + howMuch + " by the time you get there.")
