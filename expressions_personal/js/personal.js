ab11f1f97a658e4eeb0d00907de86148a3dc81de
glennodell <glennodell@fullsail.edu>
2014-01-17 21:57:56 -0600

    before math

--- expressions_personal/js/personal.js
+++ expressions_personal/js/personal.js
@@ -1,2 +1,8 @@
 // JavaScript Document
 // Glenn O'Dell 1-16-2014 expresions_personal
+
+var tank = prompt("how many gallons of fuel can your car/truck hold")
+
+var mpg = prompt("about how many miles per gallon")
+
+var cost = prompt("how much is a gallon of gas")
