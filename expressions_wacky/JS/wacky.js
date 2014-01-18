28f2fa45dd683f9fe104a7b03cf95edd9cc2dda4
glennodell <glennodell@fullsail.edu>
2014-01-17 21:48:26 -0600

    took out code

--- expressions_wacky/JS/wacky.js
+++ expressions_wacky/JS/wacky.js
@@ -12,11 +12,7 @@ var bossH =prompt("Number of epicness \n any number please");
 
 var bossHit =prompt("number of greatness \n any number please");
 
-var THP = health + armor + 10
-
-alert("The tiny humans name is " + name + " huh");
-
-alert("your total health is " + health + armor + " with your armor and you deal " + hit +" damage a hit.");
+var THP = health + armor
 
 var boss = ("The creature has " + bossH + " health, and hits for " + bossHit + " a hit");
 
