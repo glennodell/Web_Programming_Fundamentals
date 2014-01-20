1e10b3174de6fadeeab0a8ff71acbcf2ae449ee7
glennodell <glennodell@fullsail.edu>
2014-01-19 21:37:48 -0600

    console.log add

--- expressions_industry/JS/industry.js
+++ expressions_industry/JS/industry.js
@@ -14,4 +14,9 @@ var number = [rooms, computers, monitors]
 
 var totalC = number[0] * number[1]
 
-var totalM = totalC 
+var totalM = totalC * number[2]
+
+var mice = totalC
+
+console.log("You will need a total of " + totalC + " computers, and a total of " + totalM + " monitors to complete these rooms. Also you will need at total of " + mice +")
+
