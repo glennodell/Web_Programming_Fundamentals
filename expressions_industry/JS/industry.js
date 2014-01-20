9ea97dd9d8c1cf8642f310c9f204d0bc40105ec0
glennodell <glennodell@fullsail.edu>
2014-01-19 21:29:30 -0600

    started

--- expressions_industry/JS/industry.js
+++ expressions_industry/JS/industry.js
@@ -2,3 +2,6 @@
 
 //Glenn O'Dell 1-16-2014 expressions_industry
 
+// I work as tech support on a military base and we contantly have to do inventory and add computers to rooms thought this would help this make sense.
+
+var 
