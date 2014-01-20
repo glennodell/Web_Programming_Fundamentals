d6d57388bdabfe60d9a2db5832876a37dcbb4106
glennodell <glennodell@fullsail.edu>
2014-01-19 21:35:05 -0600

    before finishing all math

--- expressions_industry/JS/industry.js
+++ expressions_industry/JS/industry.js
@@ -4,4 +4,14 @@
 
 // I work as tech support on a military base and we contantly have to do inventory and add computers to rooms thought this would help this make sense.
 
-var 
+var rooms = prompt("How many rooms are you building")
+
+var computers = prompt("how many stations in each room")
+
+var monitors = prompt("how many monitors are at each station")
+
+var number = [rooms, computers, monitors]
+
+var totalC = number[0] * number[1]
+
+var totalM = totalC 
