7b5dd7a25c98c51b665e6dfea41c5bd9137abbce
glennodell <glennodell@fullsail.edu>
2014-01-23 20:03:34 -0600

    first test

--- Conditionals_Wacky/JS/Wacky.js
+++ Conditionals_Wacky/JS/Wacky.js
@@ -2,9 +2,11 @@
 
 // Glenn O'Dell	1/23/2104 Conditionals_Wacky
 
-var "sword";
-
-var "warhammer";
 
 var weapon = prompt("Do you use a sword or a warhammer?");
 
+if(weapon == sword){
+	"Your sword is made out a metal you have never seen before and feels to light to be real"
+}else{
+	"Your Warhammer is a lot light than it looks and it is covered in symbols you have never seen before."}
+	
