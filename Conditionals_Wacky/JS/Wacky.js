623709449d21b3894d7bfd9beda488ad765734fb
glennodell <glennodell@fullsail.edu>
2014-01-23 20:19:54 -0600

    added a lot more

--- Conditionals_Wacky/JS/Wacky.js
+++ Conditionals_Wacky/JS/Wacky.js
@@ -5,9 +5,28 @@
 
 var weapon = prompt("Do you use a sword or a warhammer?");
 
+if(weapon === "")
+	
+	console.log("Its dangerous to go alone you might need to pick one")
+
 if(weapon == "sword"){
+	
 	console.log("Your sword is made out a metal you have never seen before and feels to light to be real")
-}else{
+
+}
+
+if(weapon == "warhammer"){
+	
 	console.log("Your Warhammer is a lot light than it looks and it is covered in symbols you have never seen before.")
+	
 	}
+	
+var damage = prompt("your " + weapon + " does how much damage? /n 1-100")
+
+alert("You do " + damage + " Holy damage per hit"
+
+if(damage < 50){
+	console.log("It will take you a while to kill any thing")
+}else{
+	console.log("Oh look a heavy hitter")
 	
