c18166831e2b8ab1f2dfa7b0b0016deb3df27cd0
glennodell <glennodell@fullsail.edu>
2014-01-23 20:06:55 -0600

    first if else worked

--- Conditionals_Wacky/JS/Wacky.js
+++ Conditionals_Wacky/JS/Wacky.js
@@ -5,7 +5,7 @@
 
 var weapon = prompt("Do you use a sword or a warhammer?");
 
-if(weapon == sword){
+if(weapon == "sword"){
 	console.log("Your sword is made out a metal you have never seen before and feels to light to be real")
 }else{
 	console.log("Your Warhammer is a lot light than it looks and it is covered in symbols you have never seen before.")
