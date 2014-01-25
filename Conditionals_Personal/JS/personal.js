1207957cb2f1eefae438d3150bfaa11b60736444
glennodell <glennodell@fullsail.edu>
2014-01-25 15:14:33 -0600

    got my thought is order

--- Conditionals_Personal/JS/personal.js
+++ Conditionals_Personal/JS/personal.js
@@ -9,8 +9,22 @@ alert("potluck at work yay");
 //what are you bringing
 
 var Item = prompt("are you bringing chips or veggie tray")
+if(Item == ""){
+	alert("if your not going participate stop now")
+}
+if(Item == "chips"){ 
+	console.log("chips are $2.99 a bag and you need 1 bag for every 3 people.")
+}
+if(Item == "veggie tray"){
+	 console.log("veggie trays are 3.99 a tray and you need 1 for every 6 people")
+}
+// setting the price of items
 
-(Item == "chips") ? console.log("chips are $2.99 a bag and you need 1 bag for every 3 people.") : console.log("veggie trays are 3.99 a tray and you need 1 for every 6 people")
+var chips = 2.99
 
+var veggieTray = 3.99
 
-	
+//how many people are going to be there
+
+var people = prompt("how many people are attending")
+
