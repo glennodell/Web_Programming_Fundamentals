6ef52064b8be6192d8f5b7c162b5a49e16d930ca
glennodell <glennodell@fullsail.edu>
2014-01-25 14:46:18 -0600

    first

---
+++ Conditionals_Personal/JS/personal.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+//Glenn O'Dell 1/23/2104 Conditionals_Personal
+
+//potluck at work
+
+alert("potluck at work yay");
+
+//what are you bringing
+
+var Item = prompt("are you bringing chips or veggie tray")
+
+(Item == "chips") ? console.log("chips are $2.99 a bag and you need 1 bag for every 3 people.") : console.log("veggie trays are 3.99 a tray and you need 1 for every 6 people")
+
+
+	
