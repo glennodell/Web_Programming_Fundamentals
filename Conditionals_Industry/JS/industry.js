073082c9af84a064fc2b7597dd65de44adc46a4d
glennodell <glennodell@fullsail.edu>
2014-01-25 20:07:53 -0600

    typo in the html

--- Conditionals_Industry/JS/industry.js
+++ Conditionals_Industry/JS/industry.js
@@ -1,1 +1,27 @@
 // JavaScript Document
+
+// Glenn O'Dell 1/23/2014 Conditionals_Industry
+
+
+//in my job we have to replace lamps in projectors and judging by how many we have in stock and how many hours have been used on the lamp tells us when we can change them.
+
+var projector = prompt("is the projector a canon or a panasonic")
+
+if(projector == ""){
+	alert("no projector selected please start over.")
+}
+
+// what type and how many hours for each
+if(projector == "canon"){
+	console.log("you need a canon 7585 lamp to replace that lamp they are normally good for 2000 hours.")
+}
+if(projector == "panasonic"){
+	console.log("You need a 60-AW lamp to replace that lamp and they are normally good for 4000 hours.")
+}
+
+var canon = 2000
+
+var panasonic = 4000
+
+var hoursUsed = prompt("how many hours did the " + projector + " have one it?")
+
