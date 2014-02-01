3a13448667434624c6d8976519f0edf7798bf812
glennodell <glennodell@fullsail.edu>
2014-01-31 20:23:23 -0600

    trying to add a function to an if statement

--- Functions_Industry/js/industry.js
+++ Functions_Industry/js/industry.js
@@ -4,15 +4,32 @@
 
 // when building a website as part of a team how the work load need to be balanced
 
+// people vs pages
+
 var people = prompt("how many people are on your team");
 
 var pages = prompt("how many pages are required to complete the web site to the customers standards");
 
-(people < pages) ? console.log("well everyone needs to be doing more than one page for sure") : console.log("Well it looks like this will be easier with more people than pages");
+(people <= pages) ? console.log("well everyone needs to be doing more than one page for sure") : console.log("Well it looks like this will be easier with more people than pages");
 
-var average = function(people , pages){
+//average number of pages each person is responsible for
+
+function average(people , pages){
 	var total = pages / people
-	return total
+	console.log("looks like everyone needs to do a total of " + total + " page/s to complete the project");
 }
 
-console.log(average);
+average(people, pages)
+
+var js = prompt("does the customer want a lot of javascript");
+
+if(js = yes){
+	var java = function(){
+		if(people >= 10){
+			console.log("you need to make 2 people do all the Javascript")
+		}else{
+			console.log("you need to make 5 people do all the Javascript")
+		}
+	}
+}
+
