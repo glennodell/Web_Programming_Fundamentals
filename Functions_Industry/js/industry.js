1a357cc22fc41b08df373c40cd60423a410e7ef5
glennodell <glennodell@fullsail.edu>
2014-01-31 19:29:04 -0600

    first line

---
+++ Functions_Industry/js/industry.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+//Glenn O'Dell 1/28/2014 functions_indrusrty
+
+// I want to make a script the shows how often you change out lamps vs how many time you deal with a users issue
+
+var lamps = prompt("How many lamps do you change out in a week?");
+
