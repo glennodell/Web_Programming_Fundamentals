37b745f5cf4bdd6c0293c4874cec3f5256b331cb
glennodell <glennodell@fullsail.edu>
2014-01-31 19:48:09 -0600

    changes wording

--- Functions_Industry/js/industry.js
+++ Functions_Industry/js/industry.js
@@ -4,5 +4,15 @@
 
 // I want to make a script the shows how often you change out lamps vs how many time you deal with a users issue
 
-var lamps = prompt("How many lamps do you change out in a week?");
+var lamps = prompt("How many lamps do you change out this week?");
 
+var users = prompt("how many users do you assist this week?");
+
+var total = function(lamps, users){
+	var sum = lamps + users;
+	return(sum);
+}
+
+var workorders = total(lamps, users);
+
+console.log("you have gone on a total of " + workorders + " this week.")
