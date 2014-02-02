d6183ed38457a2c61670e6231b3d7bf058d93b9d
glennodell <glennodell@fullsail.edu>
2014-02-01 18:55:14 -0600

    testing

--- functions_personal/js/personal.js
+++ functions_personal/js/personal.js
@@ -5,21 +5,17 @@
 //deciding what kind of food are you going to eat and if eating in or out
 
 var where = prompt("are you eating in or out");
-
-function home(beef, chicken, pork){
+if(where === "in"){
+	var home = function(beef, chicken, pork){
 		var what = prompt("Beef, chicken or pork")
 		var howMany = prompt("how many people")
-		if(howMany === 1){
-			console.log("well atleast you dont have to get dressed.")
-		}else{
 			console.log("well i hope that the " + howMany + " can agree on it.")
-		}
-		if(what ==="Beef"){
+		if(what === "Beef"){
 			console.log("beef dinner will cost 1.25 per person")
 			var beef = 1.25
 			var sum = people * beef
 			alert("your dinner will cost you a total of $" + sum + ".")
-		 }
+	 }
 		 if(what === "chicken"){
 			 console.log("chicken dinner will cost 1.75 per person")
 			 var chicken = 1.75
@@ -31,22 +27,6 @@ function home(beef, chicken, pork){
 			 var pork = 1.15
 			 var sum = people * pork
 			 alert("your dinner will cost a total of $" + sum + ".")
-			 }
-	}
-
-if(where === "in"){
-	home
-}
-if(where === "out"){
-	function out(){
-		var where = prompt("chinese, Italian or Greek")
-		var peopleGoing = prompt("how many people are going")
-		if(peopleGoing === 1){
-			console.log("if it is just you you should have stayed in tonight")
-		}else{
-			console.log("well i hope that the " + peopleGoing + " people can agree on one place")
 		}
 	}
 }
-
-		
